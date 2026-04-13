b52fc1533e4eeb0f569cf4c04f37dbad51e6f927
CWIR3 <humumshaikh0@gmail.com>
2026-04-13 12:56:26 +0530

    Create matmul.c

---
+++ Zynq_7000_AP_SoC/Basic_Matrix_Multiplier/matmul.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xil_io.h"
+
+#define BASE_REG_ADDR	XPAR_MATRIXER_0_S00_AXI_BASEADDR
+
+int main()
+{
+    init_platform();
+
+    int temp;
+
+    printf("Matrix Multiplier Initiated \n");
+
+    while(1)
+    {
+    	printf("Enter the matrix element values for matrix A : \n");
+
+    	for(int i = 0; i <= 32; i=i+4)
+    	{
+    		scanf("%d" , &temp);
+    		Xil_Out32( (BASE_REG_ADDR + i) , temp);
+    	}
+
+    	printf("Enter the matrix element values for matrix B : \n");
+
+    	for(int i = 36; i <= 68; i=i+4)
+    	    	{
+    	    		scanf("%d" , &temp);
+    	    		Xil_Out32( (BASE_REG_ADDR + i) , temp);
+    	    	}
+
+    	printf("Printing Solution \n");
+
+
+    	for(int i = 72; i <= 80; i = i + 4)
+    	{
+    		temp = Xil_In32(BASE_REG_ADDR + i);
+    		printf("%d " , temp);
+    	}
+
+    	printf("\n");
+
+    	for(int i = 84; i <= 92; i = i + 4)
+    	    	{
+    	    		temp = Xil_In32(BASE_REG_ADDR + i);
+    	    		printf("%d " , temp);
+    	    	}
+
+    	printf("\n");
+
+    	for(int i = 96; i <= 104; i = i + 4)
+    	    	    	{
+    	    	    		temp = Xil_In32(BASE_REG_ADDR + i);
+    	    	    		printf("%d " , temp);
+    	    	    	}
+
+    	printf("\n");
+    	printf("Done \n");
+
+    }
+
+    cleanup_platform();
+    return 0;
+}
