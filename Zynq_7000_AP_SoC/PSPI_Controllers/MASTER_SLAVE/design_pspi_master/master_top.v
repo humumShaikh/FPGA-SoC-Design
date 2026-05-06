c78d394a1788ea7ad8b5eaa39267baefba2c3ac4
CWIR3 <humumshaikh0@gmail.com>
2026-05-06 15:21:04 +0530

    Add files via upload

---
+++ Zynq_7000_AP_SoC/PSPI_Controllers/MASTER_SLAVE/design_pspi_master/master_top.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/06/2026 11:37:08 AM
+// Design Name: 
+// Module Name: master_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module master_top
+(
+    input   wire            clk,
+    input   wire            reset,
+    input   wire            run,
+    input   wire    [07:00] pin,
+    input   wire            sin,
+    output  wire    [07:00] pout,
+    output  wire            sout,
+    output  wire            ss,
+    output  wire            err,
+    output  wire            sclk
+);
+
+
+    wire    wERR;
+    wire    wCLK;
+    
+    assign  err = SIPO.err;
+    assign  sclk = wCLK;
+        
+    clk_gen CLKGEN
+    (
+        .clkin(clk),
+        .reset(reset),
+        .run(run),
+        .err(wERR),
+        .clkout(wCLK),
+        .ss(ss)
+    );
+    
+    piso    PISO
+    (
+        .clk(wCLK),
+        .reset(reset),
+        .min(pin),
+        .out(sout)
+    );
+    
+    sipo    SIPO
+    (
+        .clk(wCLK),
+        .reset(reset),
+        .sin(sin),
+        .out(pout),
+        .err(wERR)
+    );
+    
+
+endmodule
